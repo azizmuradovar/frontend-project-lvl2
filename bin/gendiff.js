440ea740ce1061bb41354e9716b4be01c35f1f4d
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2020-08-17 01:43:53 +0300

    Feature: add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@ import pkg from 'commander';
 const { Command } = pkg;
 const program = new Command();
 program.version('0.0.1');
+program.arguments('<filepath1> <filepath2>')
 program.description('Compares two configuration files and shows a difference.')
 program.option('-f, --format <type>', 'output format');
 program.parse(process.argv);
