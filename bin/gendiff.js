71422b0412bb06bd177633e53fb6f035430cae2b
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2020-08-17 01:39:48 +0300

    Feature: add -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,7 @@
 import pkg from 'commander';
 const { Command } = pkg;
 const program = new Command();
-console.log(program)
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.')
+program.option('-f, --format <type>', 'output format');
 program.parse(process.argv);
