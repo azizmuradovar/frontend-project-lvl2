8e42f92884447133a6f921b73f7d4078c0376f5d
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2020-08-20 00:45:37 +0300

    Feature : add diff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,49 @@
 #!/usr/bin/env node
 import pkg from 'commander';
+import path from 'path';
+import fs from 'fs';
+import _ from 'lodash';
+
+const gendiff = (firstPath, secondPath) => {
+  const firstAbsolutePath = path.resolve(process.cwd(), firstPath);
+  const secondAbsolutePath = path.resolve(process.cwd(), secondPath);
+  const firstFileData = fs.readFileSync(firstAbsolutePath, 'utf8');
+  const secondFileData = fs.readFileSync(secondAbsolutePath, 'utf8');
+  const firstObj = JSON.parse(firstFileData);
+  const secondObj = JSON.parse(secondFileData);
+  const firstObjKeys = Object.keys(firstObj);
+  const secondObjKeys = Object.keys(secondObj);
+  const deletedKeys = _.difference(firstObjKeys, secondObjKeys);
+  const addedKeys = _.difference(secondObjKeys, firstObjKeys);
+  const uniqSortedKeys = _.uniq([...firstObjKeys, ...secondObjKeys]).sort();
+  const result = uniqSortedKeys.reduce((acc, key) => {
+    const isDeleted = deletedKeys.includes(key);
+    const isAdded = addedKeys.includes(key);
+    const isEqual = firstObj[key] === secondObj[key];
+    if (isDeleted) {
+      return (`${acc}\n  - ${key}: ${firstObj[key]}`);
+    }
+    if (isAdded) {
+      return (`${acc}\n  + ${key}: ${secondObj[key]}`);
+    }
+
+    if (isEqual) {
+      return (`${acc}\n    ${key}: ${secondObj[key]}`);
+    }
+
+    return (`${acc}\n  - ${key}: ${firstObj[key]}\n  + ${key}: ${secondObj[key]}`);
+  }, '');
+  return (`{${result}\n}`);
+};
+
 const { Command } = pkg;
 const program = new Command();
 program.version('0.0.1');
-program.arguments('<filepath1> <filepath2>')
-program.description('Compares two configuration files and shows a difference.')
+program.arguments('<firstPath> <secondPath>').action((...args) => {
+  console.log(gendiff(...args));
+});
+program.description('Compares two configuration files and shows a difference.');
 program.option('-f, --format <type>', 'output format');
 program.parse(process.argv);
+
+export default gendiff;
