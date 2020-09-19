c29c87ae8046d05ad6583c9578bd14a954262ef5
Azizmuradov Aziz <azizmuradovaziz@gmail.com>
2020-09-19 10:06:13 +0300

    Refactor: stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -25,21 +25,20 @@ const stylishFormatter = (arr, depth = 0) => {
       return stylishFormatter(tree, depth + 2);
     };
 
-    if (type === 'parent') {
-      return `${prefix}  ${key}: ${stylishFormatter(children, depth + 2)}`;
+    switch (type) {
+      case 'added':
+        return `${prefix}+ ${key}: ${getValue(value)}`;
+      case 'deleted':
+        return `${prefix}- ${key}: ${getValue(value)}`;
+      case 'unchanged':
+        return `${prefix}  ${key}: ${getValue(value)}`;
+      case 'parent':
+        return `${prefix}  ${key}: ${stylishFormatter(children, depth + 2)}`;
+      case 'changed':
+        return `${prefix}- ${key}: ${getValue(valueBefore)}\n${prefix}+ ${key}: ${getValue(valueAfter)}`;
+      default:
+        throw new Error(`Unknown node type '${type}' in stylish formatter`);
     }
-    if (type === 'changed') {
-      const deleteData = `${prefix}- ${key}: ${getValue(valueBefore)}`;
-      const addedData = `${prefix}+ ${key}: ${getValue(valueAfter)}`;
-      return `${deleteData}\n${addedData}`;
-    }
-    if (type === 'deleted') {
-      return `${prefix}- ${key}: ${getValue(value)}`;
-    }
-    if (type === 'added') {
-      return `${prefix}+ ${key}: ${getValue(value)}`;
-    }
-    return `${prefix}  ${key}: ${getValue(value)}`;
   }).join('\n');
 
   return `{\n${result}\n${endPrefix}}`;
